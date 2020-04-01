4d2689bd23e922f3694fb1fe19e3510776c3f082
Taisuke Fukuno <fukuno@jig.jp>
2020-04-01 17:36:46 +0900

    update data

--- node/git.js
+++ node/git.js
@@ -14,12 +14,17 @@ const execCommand = async function(cmd) {
 const push = async function() {
   return await execCommand('git push')
 }
+const commit = async function(mes) {
+  return await execCommand(`git commit -m '${mes}'`)
+}
 const status = async function() {
   return await execCommand('git status')
 }
 const main = async function() {
   //push()
-  console.log(await status())
+//  console.log(await status())
+//  console.log(await commit('update data'))
+  console.log(await push())
 }
 if (require.main === module) {
   main()
